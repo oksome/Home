2cc301051f9215e3caea358ed5354a35b7646a18
OKso <user+github@okso.me>
2013-08-10 17:06:35 +0200

    Started config script

---
+++ raspberry_pi/config.sh
@@ -0,0 +1,22 @@
+
+
+# Setting audio to use Jack output:
+amixer cset numid=3 1
+
+# Fixing encoding alert messages:
+sudo echo "export LC_ALL=C" >> /etc/bash.bashrc
+
+# Setting up music directory:
+mkdir $HOME/music
+sudo ln -s $HOME/music /var/lib/mpd/music/home
+sudo vim /etc/mpd.conf
+sudo service mpd restart
+
+# Setting up web server:
+mkdir $HOME/web
+sudo cp ra.nginx /etc/nginx/sites-available/ra
+cd /etc/nginx/sites-enabled
+sudo ln -s /etc/nginx/sites-available/ra ./
+sudo rm default
+cd -
+sudo service nginx reload
