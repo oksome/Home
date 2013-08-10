786571e18883a525d750aa6b1548c53e023295bc
OKso <okso.me>
2013-08-10 18:29:51 +0200

    Added Arduino code

---
+++ arduino/hawk/hawk.ino
@@ -0,0 +1,121 @@
+/*
+  Arduino code part of the Ra project. 
+  Copyright OKso http://okso.me, All rights reserved.
+  
+  This program provides a Serial interface to various Arduino functions such as:
+   - controlling remote switches via RCSwitch
+   - controlling LEDs
+   (future) - sensing temperature
+   (future) - sensing light
+  
+  Protocol:
+  - w + action: turn ON (1) of OFF (0) the current plug (eg: w0, w1)
+  - n + network ID: change current network ID (eg: n00001)
+  - p + plug ID: change current plug ID (eg: p00010)
+  - l: switch ON a LED
+  - f: switch OFF a LED
+*/
+
+#include <RCSwitch.h>
+
+RCSwitch mySwitch = RCSwitch();
+
+char inByte;
+
+/* Modes :
+- 'w': waiting for message
+- 'n': switch, waiting for network ID
+- 'p': switch, waiting for plug ID
+- 'l': switch on a LED
+- 'f': switch off a LED
+*/
+char MODE = 'w';
+
+char network_id[5+1] = "00001";
+char plug_id[5+1] = "00010";
+
+void setup() {
+  MODE = 'w';
+  
+  pinMode(9, OUTPUT); 
+  pinMode(10, OUTPUT);
+  digitalWrite(9, LOW);
+  digitalWrite(10, HIGH);
+  // Transmitter is connected to Arduino Pin #10  
+  mySwitch.enableTransmit(11);
+  
+  Serial.begin(9600);
+  Serial.println("Welcome.");
+  Serial.print("net = ");
+  Serial.println(network_id);
+  Serial.print("plug = ");
+  Serial.println(plug_id);
+}
+
+void loop() {
+
+  if (Serial.available() > 0) {
+    
+    if (MODE == 'n') {
+      Serial.readBytesUntil('\n', network_id, 5);
+      MODE = 'w';
+      Serial.print("net = ");
+      Serial.println(network_id);
+    }
+    else if (MODE == 'p') {
+      Serial.readBytesUntil('\n', plug_id, 5);
+      MODE = 'w';
+      Serial.print("plug = ");
+      Serial.println(plug_id);
+    }
+    else if (MODE == 'l') {
+      inByte = Serial.read();
+      if (inByte == '0') {
+        digitalWrite(13, HIGH);
+        MODE = 'w';
+        Serial.print("LED ON = ");
+        Serial.println(inByte);
+      }
+      else {
+        Serial.println("LED ON error");
+      }
+    }
+    else if (MODE == 'f') {
+      inByte = Serial.read();
+      if (inByte == '0') {
+        digitalWrite(13, LOW);
+        MODE = 'w';
+        Serial.print("LED OFF = ");
+        Serial.println(inByte);
+      }
+      else {
+        Serial.println("LED ON error");
+      }
+    }
+    else if (MODE == 'w') {
+      
+      inByte = Serial.read();
+      
+      if (inByte == '1') {
+        mySwitch.switchOn(network_id, plug_id);
+        Serial.println("on");
+      }
+      else if (inByte == '0') {
+        mySwitch.switchOff(network_id, plug_id);
+        Serial.println("off");
+      }
+      else if (inByte == 'w' || inByte == 'n' || inByte == 'p' || inByte == 'l' || inByte == 'f') {
+        MODE = inByte;
+        Serial.println("mode");
+      }
+      else {
+        MODE = 'w';
+        Serial.println("Debug info:");
+        Serial.print("net = ");
+        Serial.println(network_id);
+        Serial.print("plug = ");
+        Serial.println(plug_id);
+      }
+    }
+  }
+}
