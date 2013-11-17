4a6df1f7051815022d4dae625d405054b3763439
OKso <user+github@okso.me>
2013-11-17 14:55:19 +0100

    New meaning for Minion and Gadget

--- janitor/home/models.py
+++ janitor/home/models.py
@@ -1,3 +1,20 @@
+# Copyright (c) 2013 "OKso http://okso.me"
+# 
+# This file is part of Home.
+# 
+# Home is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+# 
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 from django.db import models
 from django.core.exceptions import ValidationError
 
@@ -10,6 +27,13 @@ def validate_RF433(value):
 
 class Minion(models.Model):
     '''
+    A Minion is a network endnode, connected to some input/output gadgets.
+    '''
+    name = models.CharField(max_length=64, null=False, blank=False)
+
+
+class Gadget(models.Model):
+    '''
     Common ancestor to all sensors and actuators.
     '''
     name = models.CharField(max_length=64, null=False, blank=False)
@@ -19,7 +43,7 @@ class Minion(models.Model):
         abstract = True
 
 
-class Switch(Minion):
+class Switch(Gadget):
     '''
     Controls a binary switch.
     '''
